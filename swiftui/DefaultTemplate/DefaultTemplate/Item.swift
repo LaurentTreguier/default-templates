219d3d66eb91d969d9afd0ac57b65c9aa5963675
Laurent Tréguier <laurent@treguier.org>
2023-10-12 13:01:21 +0200

    Update to XCode 15.0

---
+++ swiftui/DefaultTemplate/DefaultTemplate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DefaultTemplate
+//
+//  Created by Laurent Tréguier on 12/10/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
